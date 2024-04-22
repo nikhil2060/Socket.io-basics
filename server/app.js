3d511adc80e1a07c25b93f7d3f74691109267c60
nikhil2060 <kumarnikhil2060@gmail.com>
2024-04-22 14:33:22 +0530

    server created

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+  console.log("Id", socket.id);
+});
 
 const port = 4000;
 
