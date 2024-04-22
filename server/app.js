312d23ca71234b6d7fdea9c6f35e9e5df93f9e1c
nikhil2060 <kumarnikhil2060@gmail.com>
2024-04-22 11:50:45 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
