50dfec83839c535296431d5a9a8861208fd57320
nikhil2060 <kumarnikhil2060@gmail.com>
2024-04-23 14:01:53 +0530

    socket implemented

--- server/app.js
+++ server/app.js
@@ -5,11 +5,30 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("User connected");
   console.log("Id", socket.id);
+
+  // socket.broadcast.emit("welcome", `Welcome to the server ${socket.id} `);
+  //sends message except itself2
+
+  socket.on("message", (data) => {
+    console.log(data);
+    // socket.broadcast.emit("receive-message", data);
+    io.to(data.room).emit("receive-message", data.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
 });
 
 const port = 4000;
@@ -18,6 +37,6 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
