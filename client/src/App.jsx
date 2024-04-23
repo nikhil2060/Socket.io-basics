232e2de14aa86275c2d5b9e89742ba2206e25687
nikhil2060 <kumarnikhil2060@gmail.com>
2024-04-23 13:58:16 +0530

    implemented sockets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
+import { Button, Container, TextField, Typography, Stack } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const socket = useMemo(() => {
+    return io("http://localhost:4000");
+  }, []);
+  // const socket = io("http://localhost:4000");
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setSocketId(socket.id);
+      console.log("Connected successfully ", socket.id);
+    });
+
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+
+    socket.on("receive-message", (s) => {
+      setMessages((messages) => [...messages, s]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+    setMessage("");
+  };
+
+  // console.log(messages);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxWidth="sm">
+      <Typography variant="h6" component="div" gutterBottom>
+        {socketId}
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          label="Message"
+          value={message}
+          variant="outlined"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Room "
+          value={room}
+          variant="outlined"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+
+      <Stack>
+        {messages.map((m, i) => (
+          <Typography variant="h6" component="div" gutterBottom key={i}>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { CssBaseline } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </>
+  // </React.StrictMode>
+);
